7bc79bc6a0faf51b2c1f32c4c074a2074628a73b
tt <tt@tt>
2017-01-05 18:28:57 +0800

    修改样式

--- src/components/Movie.vue
+++ src/components/Movie.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container">
     <h1 v-text="movie.title"></h1>
-    <img :src="movie.cover" v-bind:alt="''+movie.title" v-bind:width="''+movie.cover_x/10" v-bind:height="''+ movie.cover_y/10">
+    <img :src="movie.cover" v-bind:alt="''+movie.title" v-bind:data-x="''+movie.cover_x" v-bind:data-y="''+ movie.cover_y">
     <h1>评分:{{movie.rate}}</h1>
   </div>
 </template>
